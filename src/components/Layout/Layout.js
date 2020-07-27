e5f66e102f87233d9eda11bb6edf690f3d29a3e1
Ricardo Gallardo <rgallardouk@gmail.com>
2020-07-27 15:54:22 +0100

    feat: Add stying to the Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Auxiliary from '../../hoc/Auxiliary'
+import styles from './Layout.module.css'
 
 const Layout = (props) => {
   const { children } = props
   return (
     <Auxiliary>
       <div>Toolbar, SideDrawer, Backdrop</div>
-      <main>{children}</main>
+      <main className={styles.Content}>{children}</main>
     </Auxiliary>
   )
 }
